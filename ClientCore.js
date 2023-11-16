59d9a15f0f3e1291b99585eff613a1d88dd1f373
SHIRO <58366293+CryptoEvil-Dev@users.noreply.github.com>
2023-11-16 19:37:17 +0300

    Стартуем

---
+++ ClientCore.js
@@ -0,0 +1,32 @@
+window.addEventListener("mouseover", (e) =>{
+    if(e.target.id === "header_img_1"){
+        document.querySelector(".img-holder").classList.add("paused");
+    }
+});
+window.addEventListener("mouseout", (e) =>{
+    if(e.target.id === "header_img_1"){
+        document.querySelector(".img-holder").classList.remove("paused");
+    }
+});
+
+window.addEventListener("click", (e) =>{
+    try{
+        let viewport = document.createElement("div");
+        if(e.target.id === "header_img_1"){
+            //console.log(e.target.src);
+            //let viewport = document.createElement("div");
+            viewport.innerHTML = `
+            <img id='viewport-img' class='img-viewer' src='${e.target.src}' />
+            `
+            viewport.classList.add("IMGViewer");
+            document.body.appendChild(viewport);
+            window.addEventListener("click", (e) => {
+                if(e.target.id === "viewport-img"){
+                    document.body.removeChild(viewport);
+                }
+            });
+        }
+    } catch {
+        console.log("Произошла ошибка, но это тупо заглушка)")
+    }
+})
